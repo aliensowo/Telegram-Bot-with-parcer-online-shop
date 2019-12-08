91065e99dc98a4c3b483c68885cdfc74078c084c
Belinsky <natsume.ace@gmail.com>
2019-12-08 20:23:57 +0300

    add asos base

---
+++ parser_asos.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup as BS
+
+headers = {'accept': '*/*',
+           'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:70.0) Gecko/20100101 Firefox/70.0'}
+
+base_url = 'https://www.asos.com/ru/men/ctas/aktsiya-8/cat/?cid=28240&ctaref=shop|50offparty|mw_hp_50&page=1'
+
+
+def asos_parse(base_url, headers):
+    items = []
+    session = requests.Session()
+    request = session.get(base_url, headers=headers)
+    if request.status_code == 200:
+        soup = BS(request.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'data-type': 'category'})
+        for div in divs:
+            #brand = div.find('strong', attrs={'class': 'brand-name'}).text
+            title = div.find('div', attrs={'class': 'products-list-item__brand'}).text.strip()
+            new_price = div.find('span', attrs={'class': 'price__new'}).text
+            old_price = div.find('span', attrs={'class': 'price__old'}).text
+            currency = div.find('span', attrs={'class': 'price__currency'}).text
+            items.append({
+                'title': title,
+                'new price': new_price,
+                'old prise and sale': old_price,
+                'currency': currency
+            })
+            print(title)
+            print(old_price)
+            print(new_price + ' ' + currency)
+            print("*")
+            #print(items)
+
+    else:
+        print('ERROR')
+
+
+asos_parse(base_url, headers)
