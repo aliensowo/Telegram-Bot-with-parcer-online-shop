b09dd31ebae517fb091bce6cc6e459518bacb1fe
Belinsky <natsume.ace@gmail.com>
2019-12-09 13:04:40 +0300

    echo bot

--- bot_menu.py
+++ bot_menu.py
@@ -1,13 +1,16 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 from config import TOKEN
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
 #apihelper.proxy = {'https':config.PROXY}
 
-# test commit for
+
+
 @dp.message_handler(commands=['start'])
 async def process_start_command(message: types.Message):
     await message.reply("Привет!\nДавай я тебе покажу мир скидок!")
@@ -23,5 +26,5 @@ async def echo_message(msg: types.Message):
     await bot.send_message(msg.from_user.id, msg.text)
 
 
-if __name__ == "__main__":
-    bot.polling(none_stop=True)
+if __name__ == '__main__':
+    executor.start_polling(dp)
